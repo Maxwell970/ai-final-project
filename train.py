f593ebb791e3c3a9bfe392f6cd59544cd54bb04a
Maxwell Jewell <maxwell.jewell@duke.edu>
2026-04-26 14:43:51 -0400

    training worked fixed start

--- train.py
+++ train.py
@@ -1,11 +1,83 @@
 import os
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
+import numpy as np
+import matplotlib.pyplot as plt
+
 from sb3_contrib import MaskablePPO
-from stable_baselines3.common.env_checker import check_env
 from sb3_contrib.common.maskable.utils import get_action_masks
+from stable_baselines3.common.env_checker import check_env
+from stable_baselines3.common.callbacks import BaseCallback
 
 from env import MiniRiskEnv
+
+
+class RewardCurveCallback(BaseCallback):
+    """
+    Periodically evaluates the current model during training and saves
+    average reward values so we can plot a learning curve.
+    """
+
+    def __init__(self, eval_freq=5_000, n_eval_episodes=20, verbose=1):
+        super().__init__(verbose)
+        self.eval_freq = eval_freq
+        self.n_eval_episodes = n_eval_episodes
+        self.timesteps = []
+        self.avg_rewards = []
+
+    def _on_step(self) -> bool:
+        if self.num_timesteps % self.eval_freq == 0:
+            avg_reward = self.evaluate_model()
+
+            self.timesteps.append(self.num_timesteps)
+            self.avg_rewards.append(avg_reward)
+
+            if self.verbose:
+                print(
+                    f"Evaluation at {self.num_timesteps} timesteps: "
+                    f"Average Reward = {avg_reward:.2f}"
+                )
+
+        return True
+
+    def evaluate_model(self):
+        rewards = []
+
+        for _ in range(self.n_eval_episodes):
+            env = MiniRiskEnv()
+            obs, _ = env.reset()
+
+            done = False
+            total_reward = 0
+
+            while not done:
+                action_masks = get_action_masks(env)
+
+                action, _ = self.model.predict(
+                    obs,
+                    deterministic=True,
+                    action_masks=action_masks,
+                )
+
+                obs, reward, terminated, truncated, info = env.step(action)
+
+                total_reward += reward
+                done = terminated or truncated
+
+            rewards.append(total_reward)
+
+        return np.mean(rewards)
+
+    def save_reward_curve(self, filename="training_reward_curve.png"):
+        plt.figure(figsize=(8, 5))
+        plt.plot(self.timesteps, self.avg_rewards, marker="o")
+        plt.title("PPO Learning Curve on MiniRiskEnv")
+        plt.xlabel("Training Timesteps")
+        plt.ylabel("Average Evaluation Reward")
+        plt.grid(True)
+        plt.tight_layout()
+        plt.savefig(filename)
+        plt.close()
 
 
 def describe_attack(env, attack_choice):
@@ -33,11 +105,22 @@ def train_model():
         device="cpu",
     )
 
-    model.learn(total_timesteps=100_000)
+    reward_callback = RewardCurveCallback(
+        eval_freq=5_000,
+        n_eval_episodes=20,
+        verbose=1,
+    )
+
+    model.learn(
+        total_timesteps=100_000,
+        callback=reward_callback,
+    )
 
     model.save("ppo_minirisk")
+    reward_callback.save_reward_curve("training_reward_curve.png")
 
     print("Training complete. Model saved as ppo_minirisk.zip")
+    print("Learning curve saved as training_reward_curve.png")
 
 
 def test_model():
